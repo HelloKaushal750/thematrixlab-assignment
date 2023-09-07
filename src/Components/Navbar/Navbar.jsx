c45a70b1da8e67c6cdae83bde207da95581ed23b
HelloKaushal750 <kaushalv079@gmail.com>
2023-09-07 21:38:43 +0530

    Strucutre created

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="nav_first">
+        <div>
+          <img src="Images/Vector (2).png" alt="" />
+        </div>
+        <div>
+          <img src="Images/NFTify.png" alt="" />
+        </div>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Navbar;
